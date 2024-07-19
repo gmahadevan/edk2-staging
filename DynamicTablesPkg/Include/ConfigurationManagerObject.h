NVIDIA/edk2

DynamicTablesPkg: Introduce new namespace for SMBIOS Objects
Introduce a new namespace for SMBIOS related CM Objects.

Signed-off-by: Girish Mahadevan <gmahadevan@nvidia.com>
Reviewed-by: Nick Ramirez <nramirez@nvidia.com>
Reviewed-by: Jeff Brasen <jbrasen@nvidia.com>

DynamicTablesPkg/Include/ConfigurationManagerObject.h
@@ -14,6 +14,7 @@
 
 #include <ArmNameSpaceObjects.h>
 #include <StandardNameSpaceObjects.h>
+#include <SmbiosNameSpaceObjects.h>
 
 #pragma pack(1)
 
@@ -107,7 +108,8 @@ typedef UINT32 CM_OBJECT_ID;
 typedef enum ObjectNameSpaceID {
   EObjNameSpaceStandard,      ///< Standard Objects Namespace
   EObjNameSpaceArm,           ///< ARM Objects Namespace
-  EObjNameSpaceOem = 0x8,     ///< OEM Objects Namespace
+  EObjNameSpaceOem    = 0x8,  ///< OEM Objects Namespace
+  EObjNameSpaceSmbios = 0xA,  ///< SMBIOS Objects Namespace
   EObjNameSpaceMax
 } EOBJECT_NAMESPACE_ID;
 
@@ -191,5 +193,15 @@ typedef struct CmObjDescriptor {
 **/
 #define CREATE_CM_OEM_OBJECT_ID(ObjectId) \
           (CREATE_CM_OBJECT_ID (EObjNameSpaceOem, ObjectId))
+
+/** This macro returns a Configuration Manager Object ID
+    in the SMBIOS Object Namespace.
+
+  @param [in] ObjectId    The Object ID.
+
+  @retval Returns an SMBIOS Configuration Manager Object ID.
+**/
+#define CREATE_CM_SMBIOS_OBJECT_ID(ObjectId) \
+          (CREATE_CM_OBJECT_ID (EObjNameSpaceSmbios, ObjectId))
 
 #endif // CONFIGURATION_MANAGER_OBJECT_H_

gmahadevan/edk2-staging

DynamicTablesPkg/Include/ConfigurationManagerObject.h
@@ -1,6 +1,7 @@
 /** @file
 
   Copyright (c) 2017 - 2024, Arm Limited. All rights reserved.
+  Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -15,6 +16,7 @@
 #include <ArchCommonNameSpaceObjects.h>
 #include <ArmNameSpaceObjects.h>
 #include <StandardNameSpaceObjects.h>
+#include <SmbiosNameSpaceObjects.h>
 
 #pragma pack(1)
 
@@ -83,7 +85,8 @@ typedef enum ObjectNameSpaceID {
   EObjNameSpaceStandard,          ///< Standard Objects Namespace
   EObjNameSpaceArchCommon,        ///< Arch Common Objects Namespace
   EObjNameSpaceArm,               ///< ARM Objects Namespace
-  EObjNameSpaceOem = 0xF,         ///< OEM Objects Namespace
+  EObjNameSpaceOem    = 0xF,      ///< OEM Objects Namespace
+  EObjNameSpaceSmbios,            ///< SMBIOS Objects Namespace
   EObjNameSpaceMax,
 } EOBJECT_NAMESPACE_ID;
 
@@ -177,5 +180,15 @@ typedef struct CmObjDescriptor {
 **/
 #define CREATE_CM_OEM_OBJECT_ID(ObjectId) \
           (CREATE_CM_OBJECT_ID (EObjNameSpaceOem, ObjectId))
+
+/** This macro returns a Configuration Manager Object ID
+    in the SMBIOS Object Namespace.
+
+  @param [in] ObjectId    The Object ID.
+
+  @retval Returns an SMBIOS Configuration Manager Object ID.
+**/
+#define CREATE_CM_SMBIOS_OBJECT_ID(ObjectId) \
+          (CREATE_CM_OBJECT_ID (EObjNameSpaceSmbios, ObjectId))
 
 #endif // CONFIGURATION_MANAGER_OBJECT_H_